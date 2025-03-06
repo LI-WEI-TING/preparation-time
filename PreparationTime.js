7bdadd87e0adeaf89198abf30bd1b95d633fdb5e
LI-WEI-TING <anders.li@8way.com.tw>
2025-03-06 14:29:20 +0800

    one
    one

---
+++ PreparationTime.js
@@ -0,0 +1,49 @@
+/*
+let ABC = 
+{
+  email: 'lovef2e@hexschool.com',
+  password: '12345678'
+}
+
+axios.post('https://escape-room.hexschool.io/api/user/signup',ABC)      
+    .then(function (response) {
+    console.log(response);
+    })
+    .catch(function (error) {
+    console.log(error);
+    });*/
+
+
+    let obj = 
+        {Company_ID: "EightWay"}
+      
+
+      
+axios.post('http://192.168.1.237:8022/QuickerOrder/CateList?Company_ID=EightWay') 
+
+.then(function (response) {
+ console.log(response);
+ })
+.catch(function (error) {
+  console.log(error);
+  });
+
+
+ /* axios({
+    method: 'post',
+    baseURL: 'http://192.168.1.237:8022/QuickerOrder/CateList?Company_ID=EightWay','Content-Type':'application/json',
+  })
+    .then((result) => { console.log(result.data) })
+    .catch((err) => { console.error(err) })*/  
+
+
+
+
+/*
+    axios.post('https://cors-anywhere.herokuapp.com/http://192.168.1.237:8022/QuickerOrder/CateList?Company_ID=EightWay', {
+        headers: { 'Access-Control-Allow-Origin':'*' }
+    })
+    .then(response => console.log(response.data))
+    .catch(error => console.error(error));*/
+    
+    
